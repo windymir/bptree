bde0ed59fd37c03abe3c292ce092f0e3a256aa1e
windymir <windymir@gmail.com>
2020-02-27 18:14:46 +0900

    Implement compare_key

---
+++ bptree.c
@@ -0,0 +1,34 @@
+/*
+    Author: Soeun Kim
+    Date: 27, Feb, 2020
+    Description: B+ tree
+*/
+
+#include <stdio.h>
+#include "bptree.h"
+
+
+bool compare_key(KEY a, Key b){
+    // KEY 타입 변경시 같이 수정 필요함
+    return a < b;
+}
+
+void set_order(int order) {
+    // FIXME: Implement
+}
+
+DATA get_data(KEY key) {
+    // FIXME: Implement
+}
+
+void insert_data(KEY key, DATA data){
+    // FIXME: Implement
+}
+
+void delete_data(KEY key) {
+    // FIXME: Implement
+}
+
+void show_tree() {
+    // FIXME: Implement
+}
